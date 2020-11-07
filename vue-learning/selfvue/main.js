04b0c803a6da36875192d6cf0a784293e37efbab
Zhu <scn40006@sts.saurer.vpn>
2020-11-07 21:24:53 +0800

    change selfVue.data.name to seflVue.name

--- vue-learning/selfvue/main.js
+++ vue-learning/selfvue/main.js
@@ -1,13 +1,35 @@
 
 function SelfVue(data, el, exp) {
+    var self = this;
     this.data = data;
+
+    Object.keys(data).forEach((key) => {
+        self.proxyKeys(key); // 绑定代理属性
+    });
+
     observe(data); // 对属性设置监听
     el.innerHTML = this.data[exp];
     console.log(`main.js parameter: ${this.data[exp]}`);
-    console.log(this);
+
     new Watcher(this, exp, function(value) {
-        console.log(`main.js ${value}`);
+        console.log(`main.js watch value: ${value}`);
         el.innerHTML = value;
     });
     return this;
+}
+
+SelfVue.prototype = {
+    proxyKeys: function(key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            enumerable: true,
+            configurable: true,
+            get: function proxyGetter() {
+                return self.data[key];
+            },
+            set: function proxySetter(newVal) {
+                self.data[key] = newVal;
+            }
+        })
+    }
 }
