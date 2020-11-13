c6eb42a8163171c8ac184942974055a75d5dd82f
zhu <hnscdg@126.com>
2020-11-13 15:31:07 +0800

    add test

---
+++ vue-3.0/src/views/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="test">
+        <h1>vue 3.0 experience </h1>
+        <h1>test count: {{count}}</h1>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0);
+    return { count };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.test {
+    color: red;
+}
+</style>
