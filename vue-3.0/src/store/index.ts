d09d45f97b616deeb045768f44b542188acdf71a
zhu <hnscdg@126.com>
2020-11-13 16:23:43 +0800

    add vuex and click

--- vue-3.0/src/store/index.ts
+++ vue-3.0/src/store/index.ts
@@ -2,8 +2,12 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    test: { a: 10 },
   },
   mutations: {
+    setTestA(state, value) {
+      state.test.a = value;
+    },
   },
   actions: {
   },
--- vue-3.0/src/views/Test.vue
+++ vue-3.0/src/views/Test.vue
@@ -2,16 +2,41 @@
     <div class="test">
         <h1>vue 3.0 experience </h1>
         <h1>test count: {{count}}</h1>
+        <div>count * 2 = {{doubleCount}}</div>
+        <div>state from vuex {{a}}</div>
+        <button @click="add">Add</button>
+        <button @click="update">update a</button>
     </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { computed, ref, getCurrentInstance } from 'vue';
 
 export default {
   setup() {
     const count = ref(0);
-    return { count };
+    const add = () => {
+      // eslint-disable-next-line no-plusplus
+      count.value++;
+    };
+    // watch(count.value, (val => {
+    //   console.log(`count is ${val}`);
+    // }));
+    const doubleCount = computed(() => count.value * 2);
+
+    const { ctx } = getCurrentInstance();
+    console.log(ctx.$router.currentRoute.value.path);
+    const a = computed(() => ctx.$store.state.test.a);
+    const update = () => {
+      ctx.$store.commit('setTestA', count);
+    };
+    return {
+      count,
+      add,
+      doubleCount,
+      a,
+      update,
+    };
   },
 };
 </script>
