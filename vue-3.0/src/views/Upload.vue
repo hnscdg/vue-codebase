d849efeb07e5df5da26a3f9f0d2afc1710622785
Zhu <scn40006@sts.saurer.vpn>
2020-11-26 18:21:12 +0800

    add ref sampe

--- vue-3.0/src/views/Upload.vue
+++ vue-3.0/src/views/Upload.vue
@@ -1,25 +1,42 @@
 <template>
     <div class="upload">
+      <p ref="pvalue">hello</p>
         <h1>choose a file:</h1>
-        <button @click="bntClick">choose file</button>
-        <input type="file" placeholder="please choose a file">
+        <input type="file" placeholder="please choose a file" ref="file" @change="bntClick">
+        <button @click="bntButtonClick">submit</button>
     </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 export default {
   name: 'upload',
   setup() {
-    const fileSelector = ref(null);
-    const bntClick = () => {
-      alert('click');
-      console.log(fileSelector.value);
+    // 创建DOM引用
+    const file = ref(null);
+    const pvalue = ref(null);
+
+    const bntClick = (e) => {
+      console.log(e);
     };
+
+    const bntButtonClick = (e) => {
+      console.log(e);
+    };
+
+    // DOM 首次加载之后，可以获取DOM元素的引用
+    onMounted(() => {
+      console.log(file.value.type);
+      file.value.style.color = 'red';
+      console.log(pvalue.value);
+    });
+
     return {
-      fileSelector,
+      file,
+      pvalue,
       bntClick,
+      bntButtonClick,
     };
   },
 };
